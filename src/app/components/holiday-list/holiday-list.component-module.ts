0211ad8c12ed95a3356ad497eb464aa6e28a3d9f
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-23 13:31:59 +0100

    Update holiday-list component

--- src/app/components/holiday-list/holiday-list.component-module.ts
+++ src/app/components/holiday-list/holiday-list.component-module.ts
@@ -5,10 +5,13 @@ import { FlexModule } from '@angular/flex-layout/flex';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 import { HolidayListComponent } from './holiday-list.component';
 
 @NgModule({
-  imports: [MatListModule, FlexModule, CommonModule, MatCardModule, MatButtonModule, MatMenuModule],
+  imports: [MatListModule, FlexModule, CommonModule, MatCardModule, MatButtonModule, MatMenuModule, MatFormFieldModule, MatSelectModule, MatOptionModule],
   declarations: [HolidayListComponent],
   providers: [],
   exports: [HolidayListComponent]
