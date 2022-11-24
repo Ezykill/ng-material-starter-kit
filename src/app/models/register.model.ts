19cadeb4171bb7f0461cb142c02e21082c53cd44
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-24 10:35:42 +0100

    Create register.model.ts

---
+++ src/app/models/register.model.ts
@@ -0,0 +1,13 @@
+export interface RegisterModel {
+
+  readonly email: string;
+  readonly username: string,
+  readonly password: string,
+  readonly firstname: string,
+  readonly lastname: string,
+  readonly city: string,
+  readonly street: string,
+  readonly number: string,
+  readonly zipcode: string,
+  readonly phone: string
+}
