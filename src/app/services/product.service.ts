ea5100e7922e426b5fcc6fa6ad118049fee2e3c6
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-24 11:44:22 +0100

    Update product.service.ts
    add get categories method

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProductModel } from '../models/product.model';
+import { CategoryModel } from '../models/category.model';
 
 @Injectable()
 export class ProductService {
@@ -10,5 +11,9 @@ export class ProductService {
 
   create(product: Omit<ProductModel, 'id'>): Observable<ProductModel> {
     return this._httpClient.post<ProductModel>('https://fakestoreapi.com/products', product);
+  }
+
+  getAll(): Observable<CategoryModel[]> {
+    return this._httpClient.get<CategoryModel[]>('https://fakestoreapi.com/products/categories');
   }
 }
