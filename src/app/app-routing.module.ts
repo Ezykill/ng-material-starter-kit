59ad607c3a6805540909173ec4089eac71a39943
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-27 20:10:22 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CatFactDisplayComponent } from './components/cat-fact-display/cat-fact-display.component';
+import { AgeDisplayComponent } from './components/age-display/age-display.component';
 import { ProductDetailComponentModule } from './components/product-detail/product-detail.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { CatFactDisplayComponentModule } from './components/cat-fact-display/cat-fact-display.component-module';
 import { CatFactServiceModule } from './services/cat-fact.service-module';
+import { AgeDisplayComponentModule } from './components/age-display/age-display.component-module';
+import { AgeServiceModule } from './services/age.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'product/:id', component: ProductDetailComponent }, { path: 'cat-fact', component: CatFactDisplayComponent }]), ProductDetailComponentModule, ProductServiceModule, CatFactDisplayComponentModule, CatFactServiceModule],
+  imports: [RouterModule.forRoot([{path: 'product/:id', component: ProductDetailComponent}, {
+    path: 'cat-fact',
+    component: CatFactDisplayComponent
+  }, {
+    path: 'age/:name',
+    component: AgeDisplayComponent
+  }
+  ]), ProductDetailComponentModule, ProductServiceModule, CatFactDisplayComponentModule, CatFactServiceModule, AgeDisplayComponentModule, AgeServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
