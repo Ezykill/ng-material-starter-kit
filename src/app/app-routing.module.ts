948715eeb1e75718e3f02e7cfc1f75c7e86589aa
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-27 19:43:02 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CatFactDisplayComponent } from './components/cat-fact-display/cat-fact-display.component';
 import { ProductDetailComponentModule } from './components/product-detail/product-detail.component-module';
 import { ProductServiceModule } from './services/product.service-module';
+import { CatFactDisplayComponentModule } from './components/cat-fact-display/cat-fact-display.component-module';
+import { CatFactServiceModule } from './services/cat-fact.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'product/:id', component: ProductDetailComponent }]), ProductDetailComponentModule, ProductServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'product/:id', component: ProductDetailComponent }, { path: 'cat-fact', component: CatFactDisplayComponent }]), ProductDetailComponentModule, ProductServiceModule, CatFactDisplayComponentModule, CatFactServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
