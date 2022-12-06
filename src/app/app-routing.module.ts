89ec087670c717e123f0adc4a17c7f101eb0bda2
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-06 12:53:34 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {ProductListComponentModule} from './components/product-list/product-list.component-module';
+import {ProductServiceModule} from './services/product.service-module';
+import {ProductDetailsComponentModule} from './components/product-details/product-details.component-module';
+import {ProductDetailsServiceModule} from './services/product-details.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{path: 'product-list', component: ProductListComponent}, {
+    path: 'product/:id',
+    component: ProductDetailsComponent
+  }, {
+    path: 'prod/:id',
+    component: ProductDetailsComponent
+  }]), ProductListComponentModule, ProductServiceModule, ProductDetailsComponentModule, ProductDetailsServiceModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
