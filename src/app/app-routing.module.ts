c45c6a0d780b2a032774cccfe33ac3a90008aabb
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-06 17:39:07 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CategoryChipsComponent } from './components/category-chips/category-chips.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { CategoryChipsComponentModule } from './components/category-chips/category-chips.component-module';
+import { CategoryServiceModule } from './services/category.service-module';
+import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
+import { AddProductServiceModule } from './services/add-product.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'categories', component: CategoryChipsComponent }, { path: 'add-product', component: ProductFormComponent }, { path: 'create-product', component: ProductFormComponent }]), CategoryChipsComponentModule, CategoryServiceModule, ProductFormComponentModule, AddProductServiceModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
