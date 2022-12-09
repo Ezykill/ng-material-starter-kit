97ab1d90d89d7d21fb7b242c9deeaf681d4ae5d5
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-09 03:16:29 +0100

    Create product.model.ts

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface ProductModel {
+
+  readonly id: number;
+  readonly title: number;
+  readonly price: string;
+  readonly category: string;
+  readonly description: string;
+  readonly image: string;
+
+}
