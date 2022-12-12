a930a8797e8672a6aa85794ff47f815e9f32dc12
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-12 21:05:38 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FilteredProductListComponent } from './components/filtered-product-list/filtered-product-list.component';
-import { SortedProductListComponent } from './components/sorted-product-list/sorted-product-list.component';
-import { EmployeesListComponent } from './components/employees-list/employees-list.component';
-import { FilteredProductListComponentModule } from './components/filtered-product-list/filtered-product-list.component-module';
-import { CategoriesServiceModule } from './services/categories.service-module';
-import { ProductsServiceModule } from './services/products.service-module';
-import { SortedProductListComponentModule } from './components/sorted-product-list/sorted-product-list.component-module';
-import { EmployeesListComponentModule } from './components/employees-list/employees-list.component-module';
-import { EmployeesServiceModule } from './services/employees.service-module';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {FilteredProductListComponent} from './components/filtered-product-list/filtered-product-list.component';
+import {SortedProductListComponent} from './components/sorted-product-list/sorted-product-list.component';
+import {EmployeesListComponent} from './components/employees-list/employees-list.component';
+import {
+  FilteredProductListComponentModule
+} from './components/filtered-product-list/filtered-product-list.component-module';
+import {CategoriesServiceModule} from './services/categories.service-module';
+import {ProductsServiceModule} from './services/products.service-module';
+import {SortedProductListComponentModule} from './components/sorted-product-list/sorted-product-list.component-module';
+import {EmployeesListComponentModule} from './components/employees-list/employees-list.component-module';
+import {EmployeesServiceModule} from './services/employees.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: FilteredProductListComponent }, { path: 'sorted-products', component: SortedProductListComponent }, { path: 'employees', component: EmployeesListComponent }]), FilteredProductListComponentModule, CategoriesServiceModule, ProductsServiceModule, SortedProductListComponentModule, EmployeesListComponentModule, EmployeesServiceModule],
+  imports: [RouterModule.forRoot([{path: 'products', component: FilteredProductListComponent}, {
+    path: 'sorted-products',
+    component: SortedProductListComponent
+  }, {
+    path: 'employees',
+    component: EmployeesListComponent
+  }]), FilteredProductListComponentModule, CategoriesServiceModule, ProductsServiceModule, SortedProductListComponentModule, EmployeesListComponentModule, EmployeesServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
