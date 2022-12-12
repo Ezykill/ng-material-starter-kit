1330c1a5da45fd3ad72b21638b4f641bb9300340
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-12 21:05:35 +0100

    Update employees-list.component.ts

--- src/app/components/employees-list/employees-list.component.ts
+++ src/app/components/employees-list/employees-list.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
-import { EmployeeModel } from '../../models/employee.model';
+import { PersonModel } from '../../models/person.model';
 import { EmployeesService } from '../../services/employees.service';
 import {map} from "rxjs/operators";
 
@@ -14,14 +14,14 @@ import {map} from "rxjs/operators";
 export class EmployeesListComponent {
   private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>('asc');
   public order$: Observable<string> = this._orderSubject.asObservable();
-  readonly employees$: Observable<EmployeeModel[]> = combineLatest([
+  readonly employees$: Observable<PersonModel[]> = combineLatest([
     this._employeesService.getAll(),
     this.order$
   ]).pipe(
-    map(([employees, order]: [EmployeeModel[], string]) => {
+    map(([employees, order]: [PersonModel[], string]) => {
       return employees.sort((a, b) => {
-        if(a.employee_salary > b.employee_salary) return order === 'asc' ? 1 : -1;
-        if(a.employee_salary < b.employee_salary) return order === 'asc' ? -1 : 1;
+        if(a.salary > b.salary) return order === 'asc' ? 1 : -1;
+        if(a.salary < b.salary) return order === 'asc' ? -1 : 1;
         return 0;
       })
     })
