6a8d77e1a79735c19b5402eaf268eba0763bba69
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-12 21:05:33 +0100

    Update employees.service.ts

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { EmployeeModel } from '../models/employee.model';
+import {PersonModel} from "../models/person.model";
+import {map} from "rxjs/operators";
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeesService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAll(): Observable<EmployeeModel[]> {
-    return this._httpClient.get<EmployeeModel[]>('https://dummy.restapiexample.com/employees');
+  getAll(): Observable<PersonModel[]>
+  {
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/employees').pipe(
+      map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            personalNumber: employeeResponse.id,
+            name: employeeResponse.employee_name,
+            age: employeeResponse.employee_age,
+            salary: employeeResponse.employee_salary,
+            surname: '',
+            mail: ''
+          }
+        })
+      }));
   }
 }
