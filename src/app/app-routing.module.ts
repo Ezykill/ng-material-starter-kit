3041e7ee6f9132a4b8ea488cdb5b2a823810eac9
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-27 14:51:40 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'crypto-master-details', component: CryptoListComponent }]), CryptoListComponentModule, CryptoServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
