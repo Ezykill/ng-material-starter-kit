2f7d429fe4d3f33434486905e2ae1bd771c38c73
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-12-27 17:54:33 +0100

    Update app module & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
-import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
-import { CryptoServiceModule } from './services/crypto.service-module';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CryptoListComponent} from './components/crypto-list/crypto-list.component';
+import {EmployeesListComponent} from './components/employees-list/employees-list.component';
+import {CryptoListComponentModule} from './components/crypto-list/crypto-list.component-module';
+import {CryptoServiceModule} from './services/crypto.service-module';
+import {EmployeesListComponentModule} from './components/employees-list/employees-list.component-module';
+import {EmployeesServiceModule} from './services/employees.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'crypto-master-details', component: CryptoListComponent }]), CryptoListComponentModule, CryptoServiceModule],
+  imports: [RouterModule.forRoot([{
+    path: 'crypto-master-details',
+    component: CryptoListComponent
+  }, {
+    path: 'employees-master-details',
+    component: EmployeesListComponent
+  }]), CryptoListComponentModule, CryptoServiceModule, EmployeesListComponentModule, EmployeesServiceModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
